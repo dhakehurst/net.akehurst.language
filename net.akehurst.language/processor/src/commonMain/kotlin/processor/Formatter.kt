418662187d2c52711cd09bf48ae9e2500ca7a943
David Akehurst <david.akehurst@itemis.de>
2018-11-09 20:44:01 +0100

    work on kotlin 1.3.0

---
+++ net.akehurst.language/processor/src/commonMain/kotlin/processor/Formatter.kt
@@ -0,0 +1,39 @@
+package processor
+
+class Formatter {
+    /*
+    private val formatFunctions: MutableMap<Class<*>, Function<Any, String>>
+
+    fun OglFormatter(): ??? {
+        this.formatFunctions = HashMap<Class<*>, Function<Any, String>>()
+        this.registerMethods()
+    }
+
+    internal fun registerMethods() {
+        for (m in this.javaClass.getDeclaredMethods()) {
+            if (1 == m.getParameterTypes().size) {
+                val t = m.getParameterTypes()[0]
+                val func = { o ->
+                    try {
+                        return m.invoke(this, o)
+                    } catch (e: Exception) {
+                        return e.message
+                    }
+                }
+                this.formatFunctions[t] = func
+            }
+        }
+    }
+
+    fun format(`object`: Any): String {
+        val c = `object`.javaClass
+
+        for ((key, func) in this.formatFunctions) {
+            if (key.isAssignableFrom(c)) {
+                return func.apply(`object`)
+            }
+        }
+        return `object`.toString()
+    }
+    */
+}
---
+++ net.akehurst.language/processor/src/commonMain/kotlin/processor/Ogl.kt
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2018 Dr. David H. Akehurst (http://dr.david.h.akehurst.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("Ogl")
+
+package net.akehurst.language.processor
+
+import net.akehurst.language.api.sppt2ast.Sppt2AstTransformer
+import net.akehurst.language.api.grammar.Grammar
+import net.akehurst.language.api.parser.ParseFailedException
+import net.akehurst.language.api.processor.LanguageProcessor
+import net.akehurst.language.ogl.grammar.OglGrammar
+import net.akehurst.language.ogl.sppt2ast.OglSppt2AstTransformer
+import kotlin.js.JsName
+import kotlin.jvm.JvmName
+
+
+private val oglProcessor: LanguageProcessor by lazy {
+    val grammar = OglGrammar()
+    val sppt2ast: Sppt2AstTransformer = OglSppt2AstTransformer()
+    processor(grammar, sppt2ast)
+}
+
+@JsName("processorFromGrammar")
+fun processor(grammar: Grammar): LanguageProcessor {
+    return LanguageProcessorDefault(grammar, null)
+}
+
+@JsName("processorFromGrammarWithSemanticAnalyser")
+fun processor(grammar: Grammar, semanticAnalyser: Sppt2AstTransformer): LanguageProcessor {
+    return LanguageProcessorDefault(grammar, semanticAnalyser)
+}
+
+@JsName("processor")
+fun processor(grammarDefinitionStr: String): LanguageProcessor {
+    try {
+        val grammar = oglProcessor.process<Grammar>("grammarDefinition", grammarDefinitionStr)
+        return processor(grammar)
+    } catch (e: ParseFailedException) {
+        //TODO: better, different exception to detect which list item fails
+        throw ParseFailedException("Unable to parse grammarDefinitionStr ", e.longestMatch, e.location)
+    }
+}
+
+@JsName("processorWithSemanticAnalyser")
+fun processor(grammarDefinitionStr: String, semanticAnalyser: Sppt2AstTransformer): LanguageProcessor {
+    try {
+        val grammar = oglProcessor.process<Grammar>("grammarDefinition", grammarDefinitionStr)
+        return processor(grammar, semanticAnalyser)
+    } catch (e: ParseFailedException) {
+        //TODO: better, different exception to detect which list item fails
+        throw ParseFailedException("Unable to parse grammarDefinitionStr ", e.longestMatch, e.location)
+    }
+}
+
+@JsName("processorFromRuleList")
+fun processor(rules: List<String>): LanguageProcessor {
+    val prefix = "namespace temp grammar Temp { "
+    val grammarStr = prefix + rules.joinToString(" ") + "}"
+    try {
+        val grammar = oglProcessor.process<Grammar>("grammarDefinition", grammarStr)
+        return LanguageProcessorDefault(grammar, null)
+    } catch (e: ParseFailedException) {
+        //TODO: better, different exception to detect which list item fails
+        val newCol = e.location["column"]?.minus(prefix.length) ?: 0
+        val location = mapOf<String, Int>(Pair("line", 1), Pair("column", newCol))
+        throw ParseFailedException("Unable to parse list of rules", e.longestMatch, location)
+    }
+}
+
+
