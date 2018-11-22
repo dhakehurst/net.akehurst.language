3cfba0999b86d423f8a6ca9fc8122ccfeb482067
David Akehurst <david.akehurst@itemis.de>
2018-11-22 14:12:53 +0100

    work on kotlin 1.3.0

---
+++ net.akehurst.language/processor/src/commonTest/kotlin/parser/scannerless/test_RuntimeParser_parse_choicePriority.kt
@@ -0,0 +1,343 @@
+/**
+ * Copyright (C) 2018 Dr. David H. Akehurst (http://dr.david.h.akehurst.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.akehurst.language.parser.scannerless
+
+import net.akehurst.language.api.parser.ParseFailedException
+import net.akehurst.language.api.sppt.SharedPackedParseTree
+import net.akehurst.language.ogl.runtime.structure.RuntimeRuleSet
+import net.akehurst.language.ogl.runtime.structure.RuntimeRuleSetBuilder
+import kotlin.test.*
+
+class test_RuntimeParser_parse_choicePriority {
+
+    private fun test_parse(sp: ScannerlessParser, goalRuleName: String, inputText: String): SharedPackedParseTree {
+        return sp.parse(goalRuleName, inputText)
+    }
+
+    // r = a < b < c;
+    // a = 'a' ;
+    // b = 'b' ;
+    // c = 'c' ;
+    private fun aObOcLiteral(): ScannerlessParser {
+        val b = RuntimeRuleSetBuilder()
+        val r1 = b.rule("a").concatenation(b.literal("a"))
+        val r2 = b.rule("b").concatenation(b.literal("b"))
+        val r3 = b.rule("c").concatenation(b.literal("c"))
+        b.rule("r").choicePriority(r1, r2, r3)
+        val sp = ScannerlessParser(b.ruleSet())
+        return sp
+    }
+
+    @Test
+    fun aObOcLiteral_r_empty_fails() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = ""
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(0, ex.location.column)
+    }
+
+    @Test
+    fun aObOcLiteral_r_a() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "a"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun aObOcLiteral_r_b() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "b"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun aObOcLiteral_r_c() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "c"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun aObOcLiteral_r_d_fails() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "d"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(0, ex.location.column)
+    }
+
+    @Test
+    fun aObOcLiteral_r_ab_fails() {
+        val sp = this.aObOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "ab"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(1, ex.location.column)
+    }
+
+    // r = a b < c;
+    // a = 'a' ;
+    // b = 'b' ;
+    // c = 'c' ;
+    private fun abOcLiteral(): ScannerlessParser {
+        val b = RuntimeRuleSetBuilder()
+        val ra = b.rule("a").concatenation(b.literal("a"))
+        val rb = b.rule("b").concatenation(b.literal("b"))
+        val rc = b.rule("c").concatenation(b.literal("c"))
+        val rab = b.rule("ab").concatenation(ra, rb)
+        b.rule("r").choicePriority(rab, rc)
+        val sp = ScannerlessParser(b.ruleSet())
+        return sp
+    }
+
+    @Test
+    fun abOcLiteral_r_empty_fails() {
+        val sp = this.abOcLiteral()
+        val goalRuleName = "r"
+        val inputText = ""
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(0, ex.location.column)
+    }
+
+    @Test
+    fun abOcLiteral_r_a_fails() {
+        val sp = this.abOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "a"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(1, ex.location.column)
+    }
+
+    @Test
+    fun abOcLiteral_r_ab() {
+        val sp = this.abOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "ab"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun abOcLiteral_r_abc_fails() {
+        val sp = this.abOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "abc"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(2, ex.location.column)
+    }
+
+    @Test
+    fun abOcLiteral_r_c() {
+        val sp = this.abOcLiteral()
+        val goalRuleName = "r"
+        val inputText = "c"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    // r = a < b c;
+    // a = 'a' ;
+    // b = 'b' ;
+    // c = 'c' ;
+    private fun aObcLiteral(): ScannerlessParser {
+        val b = RuntimeRuleSetBuilder()
+        val ra = b.rule("a").concatenation(b.literal("a"))
+        val rb = b.rule("b").concatenation(b.literal("b"))
+        val rc = b.rule("c").concatenation(b.literal("c"))
+        val rbc = b.rule("bc").concatenation(rb, rc)
+        b.rule("r").choicePriority(ra, rbc)
+        val sp = ScannerlessParser(b.ruleSet())
+        return sp
+    }
+
+    @Test
+    fun aObcLiteral_r_empty_fails() {
+        val sp = this.aObcLiteral()
+        val goalRuleName = "r"
+        val inputText = ""
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(0, ex.location.column)
+    }
+
+    @Test
+    fun aObcLiteral_r_a() {
+        val sp = this.aObcLiteral()
+        val goalRuleName = "r"
+        val inputText = "a"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun aObcLiteral_r_ab_fails() {
+        val sp = this.aObcLiteral()
+        val goalRuleName = "r"
+        val inputText = "ab"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(2, ex.location.column)
+    }
+
+    @Test
+    fun aObcLiteral_r_abc_fails() {
+        val sp = this.aObcLiteral()
+        val goalRuleName = "r"
+        val inputText = "abc"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(2, ex.location.column)
+    }
+
+    @Test
+    fun aObcLiteral_r_bc() {
+        val sp = this.aObcLiteral()
+        val goalRuleName = "r"
+        val inputText = "bc"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    // r = a1 b < a2;
+    // a1 = 'a' ;
+    // b = 'b' ;
+    // a2 = 'a' ;
+    private fun a1bOa2Literal(): ScannerlessParser {
+        val b = RuntimeRuleSetBuilder()
+        val r_a1 = b.rule("a1").concatenation(b.literal("a"))
+        val r_b = b.rule("b").concatenation(b.literal("b"))
+        val r_a2 = b.rule("a2").concatenation(b.literal("a"))
+        val r_a1b = b.rule("a1b").concatenation(r_a1, r_b)
+        b.rule("S").choicePriority(r_a1b, r_a2)
+        val sp = ScannerlessParser(b.ruleSet())
+        return sp
+    }
+
+    @Test
+    fun a1bOa2Literal_r_empty_fails() {
+        val sp = this.a1bOa2Literal()
+        val goalRuleName = "S"
+        val inputText = ""
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(0, ex.location.column)
+    }
+
+    @Test
+    fun a1bOa2Literal_r_a() {
+        val sp = this.a1bOa2Literal()
+        val goalRuleName = "S"
+        val inputText = "a"
+
+        test_parse(sp, goalRuleName, inputText)
+
+    }
+
+    need to test priority choice more, i.e. 'if then else | if then' & 'int int'
+
+    @Test
+    fun a1bOa2Literal_r_ab() {
+        val sp = this.a1bOa2Literal()
+        val goalRuleName = "S"
+        val inputText = "ab"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun a1bOa2Literal_r_abc_fails() {
+        val sp = this.a1bOa2Literal()
+        val goalRuleName = "S"
+        val inputText = "abc"
+
+        val ex = assertFailsWith(ParseFailedException::class) {
+            test_parse(sp, goalRuleName, inputText)
+        }
+        assertEquals(1, ex.location.line)
+        assertEquals(2, ex.location.column)
+    }
+
+    @Test
+    fun a1bOa2Literal_r_c() {
+        val sp = this.a1bOa2Literal()
+        val goalRuleName = "S"
+        val inputText = "c"
+
+        val actual = test_parse(sp, goalRuleName, inputText)
+
+        assertNotNull(actual)
+    }
+}
---
+++ net.akehurst.language/processor/src/commonTest/kotlin/processor/ambiguity/test_PascalRange.kt
@@ -0,0 +1,81 @@
+package processor.ambiguity
+
+import net.akehurst.language.api.grammar.Grammar
+import net.akehurst.language.api.parser.ParseFailedException
+import net.akehurst.language.ogl.ast.GrammarBuilderDefault
+import net.akehurst.language.ogl.ast.NamespaceDefault
+import net.akehurst.language.ogl.runtime.structure.RuntimeRuleItem
+import net.akehurst.language.ogl.runtime.structure.RuntimeRuleItemKind
+import net.akehurst.language.ogl.runtime.structure.RuntimeRuleSetBuilder
+import processor.test_ParserAbstract
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class test_PascalRange : test_ParserAbstract() {
+
+    /*
+     * expr : range | real ;
+     * range: integer '..' integer ;
+     * integer : "[0-9]+" ;
+     * real : "([0-9]+[.][0-9]*)|([.][0-9]+)" ;
+     *
+     */
+    private fun S(): RuntimeRuleSetBuilder {
+        val rrb = RuntimeRuleSetBuilder()
+        val r_real = rrb.rule("real").concatenation(rrb.pattern("([0-9]+[.][0-9]*)|([.][0-9]+)"))
+        val r_integer = rrb.rule("integer").concatenation(rrb.pattern("[0-9]+"))
+        val r_range = rrb.rule("range").concatenation(r_integer, rrb.literal(".."), r_integer)
+        val r_expr = rrb.rule("expr").choiceEqual(r_range, r_real)
+        return rrb
+    }
+
+    @Test
+    fun expr_empty() {
+        val rrb = this.S()
+        val goal = "expr"
+        val sentence = ""
+
+        val e = assertFailsWith(ParseFailedException::class) {
+            super.test(rrb, goal, sentence)
+        }
+        assertEquals(1, e.location.line)
+        assertEquals(0, e.location.column)
+    }
+
+    @Test
+    fun expr_1() {
+        val rrb = this.S()
+        val goal = "expr"
+        val sentence = "1."
+
+        val expected1 = """
+            expr {
+              real { '([0-9]+[.][0-9]*)|([.][0-9]+)' : '1.' }
+            }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1)
+
+    }
+
+    @Test
+    fun expr_1_to_5() {
+        val rrb = this.S()
+        val goal = "expr"
+        val sentence = "1..5"
+
+        val expected1 = """
+            expr {
+              range {
+                integer { '[0-9]+' : '1' }
+                '..'
+                integer { '[0-9]+' : '5' }
+              }
+            }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1)
+
+    }
+}
