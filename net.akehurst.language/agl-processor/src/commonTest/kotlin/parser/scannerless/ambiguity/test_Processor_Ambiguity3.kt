721a06d1d709ee694a04de2b15ff29f50fe7c024
David Akehurst <david.akehurst@itemis.de>
2019-02-06 11:09:33 +0100

    adding a test

---
+++ net.akehurst.language/agl-processor/src/commonTest/kotlin/parser/scannerless/ambiguity/test_Processor_Ambiguity3.kt
@@ -0,0 +1,229 @@
+/**
+ * Copyright (C) 2018 Dr. David H. Akehurst (http://dr.david.h.akehurst.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.akehurst.language.parser.scannerless.ambiguity
+
+import net.akehurst.language.api.parser.ParseFailedException
+import net.akehurst.language.agl.runtime.structure.RuntimeRuleItem
+import net.akehurst.language.agl.runtime.structure.RuntimeRuleItemKind
+import net.akehurst.language.agl.runtime.structure.RuntimeRuleSetBuilder
+import net.akehurst.language.parser.scannerless.test_ScannerlessParserAbstract
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class test_Processor_Ambiguity3 : test_ScannerlessParserAbstract() {
+    /**
+     * From [https://pdfs.semanticscholar.org/eeac/392e02671b0edcd81ae080a5117e5f9584f5.pdf]
+     * Generalised Parsing: Some Costs. Adrian Johnstone, Elizabeth Scott, and Giorgios Economopoulos
+     * S = A b | A' c ;
+     * A' = A' a | a ;
+     * A  = a A | A a | a ;
+     */
+    /**
+     * S = S1 | S2 ;
+     * S1 = P 'b' ;
+     * S2 = Q 'c' ;
+     * Q = Q1 | 'a' ;
+     * Q1 = Q 'a'
+     * P  = a P | P a | a ;
+     */
+    private fun S(): RuntimeRuleSetBuilder {
+        val b = RuntimeRuleSetBuilder()
+        val r_Q1 = b.rule("Q1").concatenation(r_Q, r_a)
+        val r_Q = b.rule("Q").choiceEqual(r_Q1, r_a)
+        val r_S2 = b.rule("S2").concatenation(r_Q, r_c)
+        val r_S1 = b.rule("S1").concatenation(r_P, r_b)
+        b.rule("S").choiceEqual(r_S1, r_S2)
+        return b
+    }
+
+    @Test
+    fun S_S_empty() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = ""
+
+        val e = assertFailsWith(ParseFailedException::class) {
+            super.test(rrb, goal, sentence)
+        }
+        assertEquals(1, e.location.line)
+        assertEquals(0, e.location.column)
+    }
+
+    @Test
+    fun S_S_a() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = "a"
+
+        val expected1 = """
+            S { 'a' }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1)
+    }
+
+    @Test
+    fun S_S_aa() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = "aa"
+
+        val expected1 = """
+            S {
+              S1 {
+                'a'
+                S { 'a' }
+                B { §empty }
+                B { §empty }
+              }
+            }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1)
+    }
+
+    @Test
+    fun S_S_aab() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = "aab"
+
+        val expected1 = """
+            S {
+              S1 {
+                'a'
+                S { 'a' }
+                B { 'b' }
+                B { §empty }
+              }
+            }
+        """.trimIndent()
+
+        val expected2 = """
+            S {
+              S1 {
+                'a'
+                S { 'a' }
+                B { §empty }
+                B { 'b' }
+              }
+            }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1, expected2)
+    }
+
+    @Test
+    fun S_S_aabb() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = "aabb"
+
+        val expected1 = """
+            S {
+              S1 {
+                'a'
+                S { 'a' }
+                B { 'b' }
+                B { 'b' }
+              }
+            }
+        """.trimIndent()
+
+        super.test(rrb, goal, sentence, expected1)
+    }
+
+    @Test
+    fun S_S_aaabb() {
+        val rrb = this.S()
+        val goal = "S"
+        val sentence = "aaabb"
+
+        val expected1 = """
+            S { S1 {
+              'a'
+              S { S1 {
+                  'a'
+                  S { 'a' }
+                  B { 'b' }
+                  B { §empty }
+              } }
+              B { 'b' }
+              B { §empty }
+            } }
+        """.trimIndent()
+
+        val expected2 = """
+            S { S1 {
+              'a'
+              S { S1 {
+                  'a'
+                  S { 'a' }
+                  B { §empty }
+                  B { 'b' }
+              } }
+              B { 'b' }
+              B { §empty }
+            } }
+        """.trimIndent()
+
+        val expected3 = """
+            S { S1 {
+              'a'
+              S { S1 {
+                  'a'
+                  S { 'a' }
+                  B { 'b' }
+                  B { §empty }
+              } }
+              B { §empty }
+              B { 'b' }
+            } }
+        """.trimIndent()
+
+        val expected4 = """
+            S { S1 {
+              'a'
+              S { S1 {
+                  'a'
+                  S { 'a' }
+                  B { §empty }
+                  B { 'b' }
+              } }
+              B { §empty }
+              B { 'b' }
+            } }
+        """.trimIndent()
+
+        val expected5 = """
+            S { S1 {
+              'a'
+              S { S1 {
+                  'a'
+                  S { 'a' }
+                  B { 'b' }
+                  B { 'b' }
+              } }
+              B { §empty }
+              B { §empty }
+            } }
+        """.trimIndent()
+
+        super.testStringResult(rrb, goal, sentence, expected1, expected2, expected3, expected4, expected5)
+    }
+}
