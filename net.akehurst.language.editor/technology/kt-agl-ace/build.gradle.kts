efafa3e57f89ddea2f81ca3080121f2e70cce9e0
David Akehurst <david.akehurst@itemis.de>
2020-03-23 08:21:36 +0100

    missing files

--- net.akehurst.language.editor/technology/kt-agl-ace/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-
-val version_agl:String by project
-val version_ace:String = "1.4.8"
-dependencies {
-
-    jsMainImplementation("net.akehurst.language:agl-processor:$version_agl")
-    jsMainImplementation(npm("ace-builds", version_ace))
-}
