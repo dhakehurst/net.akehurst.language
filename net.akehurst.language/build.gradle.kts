39017cef816e486e5a1f4f5f27b3c0cd7638a3c1
Dr D H Akehurst <dhakehurst@users.noreply.github.com>
2024-12-13 20:43:52 +0100

    kotlin 2.1.0

--- net.akehurst.language/build.gradle.kts
+++ net.akehurst.language/build.gradle.kts
@@ -42,8 +42,8 @@ allprojects {
 }
 
 subprojects {
-    val kotlin_languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
-    val kotlin_apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+    val kotlin_languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1
+    val kotlin_apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1
     val jvmTargetVersion = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
 
     apply(plugin = "org.jetbrains.kotlin.multiplatform")
